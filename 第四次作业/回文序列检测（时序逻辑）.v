b8f6ebdbe162f133386c0f33ef5f59bed10d9d02
耀天 闫 <3219412675@qq.com>
2025-10-28 08:51:36 +0800

    commit

--- 第四次作业/回文序列检测（时序逻辑）.v
+++ 第四次作业/回文序列检测（时序逻辑）.v
@@ -1,6 +1,6 @@
 module PalindromicSequenceDetector(
     input wire CLK,   // 时钟信号
-    input wire In,    // 数据输入
+    input wire IN,    // 数据输入
     output wire OUT   // 输出回文检测结果
 );
     // 使用一个5位宽的寄存器来存储输入数据，初始化为0
@@ -10,11 +10,11 @@ module PalindromicSequenceDetector(
     always @(posedge CLK) begin
         if (count < 5) begin
             // 只在寄存器未满时更新寄存器并计数
-            r <= {r[3:0], In};  // 将寄存器r的前四位右移一位，并将当前输入In放入最低位
+            r <= {r[3:0], IN};  // 将寄存器r的前四位右移一位，并将当前输入In放入最低位
             count <= count + 1;  // 增加计数
         end else begin
             // 一旦寄存器已满，继续更新，但不再计数
-            r <= {r[3:0], In};  // 将寄存器r的前四位右移一位，并将当前输入In放入最低位
+            r <= {r[3:0], IN};  // 将寄存器r的前四位右移一位，并将当前输入IN放入最低位
         end
     end
 
