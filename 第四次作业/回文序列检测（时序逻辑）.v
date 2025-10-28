b5115403b60ff9aef51fdd1f097c5d9333707848
耀天 闫 <3219412675@qq.com>
2025-10-28 09:28:47 +0800

    commit

--- 第四次作业/回文序列检测（时序逻辑）.v
+++ 第四次作业/回文序列检测（时序逻辑）.v
@@ -6,18 +6,14 @@ module PalindromicSequenceDetector(
     
     reg [4:0] r = 0;  
     reg [2:0] count = 0;  
-
     always @(posedge CLK) begin
         if (count < 5) begin
-           
             r <= {r[3:0], IN};  
             count <= count + 1;  
         end else begin
-            
             r <= {r[3:0], IN};  
         end
     end
-
     assign OUT = (count >= 5) && (r[4] == r[0]) && (r[3] == r[1]);
 
 endmodule
