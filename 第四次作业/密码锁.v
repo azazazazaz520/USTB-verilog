f2c631dc440e7a7708263d45cff92025f1aa4d05
耀天 闫 <3219412675@qq.com>
2025-10-30 16:05:55 +0800

    commit

--- 第四次作业/密码锁.v
+++ 第四次作业/密码锁.v
@@ -10,17 +10,16 @@ parameter [15:0] stu_pwd = 16'b0001 0001 0111 1011;//4475
 localparam IDLE =2'b00;
 localparam INPUT =2'b01;//输入状态
 localparam CHECK =2'b10;//校验状态
+reg [3:0] bit_cnt;//记录当前输入的位数
 reg [3:0] temp; //临时存储输入的1位密码
-reg [15:0] input_pwd; //存储输入的密码
-reg [2:0] cnt;
-reg alock;
+reg [15:0] input_seq; //存储输入的密码
+reg [2:0] num_cnt;//记录当前已存入的数字个数
 reg [1:0] state; //状态机
 reg input_done;//输入完成标志
 always @(posedge clk or posedge rst) begin
     if(rst) begin
-        temp = arst;
-        input_pwd = arst;
-        state = IDLE;
+        
+        state <= IDLE;
     end
 end
 endmodule
