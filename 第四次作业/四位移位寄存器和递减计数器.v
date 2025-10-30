b8218aa7f3939415c18391acbe7aadd9d1a761ea
耀天 闫 <3219412675@qq.com>
2025-10-30 08:14:06 +0800

    commit

--- 第四次作业/四位移位寄存器和递减计数器.v
+++ 第四次作业/四位移位寄存器和递减计数器.v
@@ -6,7 +6,18 @@ module top_module (
     input data,
     output [3:0] q
 );
+reg [3:0] output_q;
+always @(posedge clk or posedge reset) begin
+    if(reset) begin
+        output_q <= 4'b0000;
+    end
+    else if(shift_ena) begin
+        output_q <= {output_q[2:0],data};
+    end
+    else if(count_ena) begin
+        output_q <= output_q - 1;
+    end
 
-
-
+end
+assign q = output_q;
 endmodule
